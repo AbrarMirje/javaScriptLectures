9ca6e0dc48dacf871e406bb63a51394146a9b45e
Abrar <abrarmirje5@gmail.com>
2023-12-03 08:24:55 +0530

    added variables

---
+++ script.js
@@ -0,0 +1,85 @@
+// document.querySelector("#head").style.color = "red";
+
+// class .
+// id #
+
+// document.querySelector("#head").style.display = "none";
+
+// document.querySelector("#head").style.display = "block";
+
+
+// document.querySelector("#head").innerHTML = "Welcome";
+
+
+// Variables
+// String "" number 01245 boolean true false
+
+// var promote re-declaration as well as reassignment
+var name = "js";
+var name;
+name = "Pune"
+
+// console.log(name);
+// console.log(name);
+// console.log(typeof name);
+
+
+// let promotes only reassignment not re-declaration
+let username;
+username = 369;
+// console.log(username);
+// console.log(typeof username);
+
+
+// Nahi karu shakat --> only declaration, reassignment, redeclaration
+const customerName = 25; //best approach
+// const customerName = 123;
+// customerName = 258;
+
+
+// console.log(customerName);
+// console.log(typeof customerName);
+
+
+customerId = 123;
+// console.log(customerId);
+
+
+// Hoisted
+
+// console.log(contactNumber);
+// var contactNumber = 25365;
+
+
+// console.log(abc);
+// let abc = 258;
+
+
+
+
+
+var shiroli = 12345;
+// Scope of variables
+
+function village() {
+    // console.log(shiroli);
+}
+// {
+//     {
+//         {
+//             {
+//                 {
+//                     {
+//                         console.log(shiroli);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+console.log(`Baaher ${shiroli}`);
+
+village();
+
+// overapi
