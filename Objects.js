efaec9f069079ba465b15e2dccb4cc73709aa52b
Abrar <abrarmirje5@gmail.com>
2023-12-08 18:30:02 +0530

    added Object

---
+++ Objects.js
@@ -0,0 +1,34 @@
+// // const userName = "JS";
+
+
+// key === value
+const userNames = {
+    firstName: "Abrar",
+    lastName: "Mirje",
+    location: "Shiroli"
+}
+
+// console.log(userNames.firstName);
+// // console.log(firstName);
+console.log(userNames);
+console.log(delete userNames.lastName);
+console.log(userNames);
+
+// let university = {
+//     name: 'DYPATU',
+//     location: 'Talsande',
+//     established: '2023',
+
+
+//     displayInfo: function () {
+//         console.log(`${university.name} was established
+//     in ${university.established} at ${university.location}`
+//         );
+//     }
+// }
+// university.displayInfo();
+
+
+// BuiltIn
+// toString();
+// parseFloat();
